8a4b5b73ff15f22e67b08b592d96c4e8e52a11ac
Felix Jumason <71150929+Blackie360@users.noreply.github.com>
2024-03-06 18:57:44 +0000

    fixed typo className

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -31,7 +31,7 @@ export default function Auth() {
           <form onSubmit={handleLogin}>
             <input type="email" 
               name="email" 
-              class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
               placeholder="your@email.com"
               id="website"
               value={email}
